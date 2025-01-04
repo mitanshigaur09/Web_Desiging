dc0b479b88f5273d33fb8b1e4dd29c1759c017fc
Mitanshi Gaur <118531410+mitanshigaur09@users.noreply.github.com>
2025-01-04 20:39:47 +0530

    Add files via upload

---
+++ Green Tree Arbourist/home.php
@@ -0,0 +1,235 @@
+<?php
+/*
+
+Template Name: Home
+
+*/
+
+get_header();
+?>
+
+<div class="hero-section">
+	<div id="hero-slider" class="owl-carousel owl-loaded owl-drag">
+		<div class="home_bnr_item">
+			<img  class="dstop_bnner" src="<?php echo get_template_directory_uri(); ?>/images/tree-banner.jpg" alt=""/>
+			<div class="banner-content">
+				<h1>Tree Condition & Survey <br>Reports </h1>
+				<!-- <h1>Reports</h1> -->
+				<p>Mitigate risks & Avoid unnecessary costs</p>
+				<ul class="header-content" type="square">
+					<li>Lorem ipsum dolor sit amet, consectetur.</li>
+					<li>Curabitur ornare augue id dui tincidunt facilisis.</li>
+					<li>Nam et nisl vehicula, sodales lorem.</li>
+				</ul>
+				<button type="button" class="btn" >Request A Consultation</button>
+
+			</div>
+		</div>
+		<div class="home_bnr_item">
+			<img  class="dstop_bnner" src="<?php echo get_template_directory_uri(); ?>/images/tree-banner.jpg" alt=""/>
+			<div class="banner-content">
+				
+				<h1>Tree Condition & Survey <br>Reports  </h1>
+				<!-- <h1>Reports</h1> -->
+				<p>Mitigate risks & Avoid unnecessary costs</p>
+				<ul class="header-content" type="square">
+					<li>Lorem ipsum dolor sit amet, consectetur.</li>
+					<li>Curabitur ornare augue id dui tincidunt facilisis.</li>
+					<li>Nam et nisl vehicula, sodales lorem.</li>
+				</ul>
+				<button type="button" class="btn" >Request A Consultation</button>
+			</div>
+		</div>
+	</div>
+	<div class="strip">
+		<div id="icon-script" class="owl-carousel owl-loaded owl-drag">
+			<div class="scropt-img">
+				<img src="<?php echo get_template_directory_uri() . '/images/script11.png'; ?>" />
+				<p>Competitive Costs</p>
+			</div>
+			<div class="scropt-img">
+				<img src="<?php echo get_template_directory_uri() . '/images/script22.png'; ?>" />
+				<p>23 Years Experience</p>
+			</div>
+			<div class="scropt-img">
+				<img src="<?php echo get_template_directory_uri() . '/images/script33.png'; ?>" />
+				<p>Fully Insured</p>
+			</div>
+
+		</div>
+	</div>
+
+	<div class="trust">
+		<div class="container">
+			<div class="all-heading"><h4>Trusted By</h4></div>
+			<div id="trusred-slider" class="owl-carousel owl-loaded owl-drag">
+				<div class="trusted-img">
+					<img src="<?php echo get_template_directory_uri() . '/images/frame1.png'; ?>" />
+				</div>
+				<div class="trusted-img">
+					<img src="<?php echo get_template_directory_uri() . '/images/frame2.png'; ?>" />
+				</div>
+				<div class="trusted-img">
+					<img src="<?php echo get_template_directory_uri() . '/images/frame3.png'; ?>" />
+				</div>
+
+				<div class="trusted-img">
+					<img src="<?php echo get_template_directory_uri() . '/images/frame4.png'; ?>" />
+				</div>
+
+				<div class="trusted-img">
+					<img src="<?php echo get_template_directory_uri() . '/images/frame5.png'; ?>" />
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<div class="container">
+					<div class="home-bring">
+			<div class="bring-contant">
+				
+				<div class="all-heading">
+					<h2>About Us</h2>
+				</div>
+
+				<h4> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin mollis justo orci, non semper orci fringilla nec. Fusce interdum sapien eros, sit amet.</h4>
+			<p>Vivamus eleifend nibh efficitur, lobortis velit nec, dapibus tellus. Praesent id ullamcorper sapien. Fusce felis dui, maximus tempor mi sit amet, fermentum viverra felis. Quisque finibus arcu sit amet urna mollis ultricies.</p>				<ul>
+				<li>Praesent a libero in nulla convallis</li>
+				<li>Tincidunt a id lorem mauris lobortis lobortis</li>
+				<li>Morbi sem augue, condimentum quis </li>
+				
+			</ul>				<div class="all-btn">
+					<a href="#">Request a Consultation</a>
+				</div>
+			</div>
+
+			<div class="bring-img">
+				<img src="<?php echo get_template_directory_uri() . '/images/abt_green.png'; ?>" />
+			</div>
+		</div>
+	
+
+</div>
+<div class="key">
+<div class="container">
+	<div class="projects-section">
+		<div class="all-heading">
+					<h2>Key Services</h2>
+				</div>
+
+		<div class="projects-row">
+								<div class="projects-col">
+						<div class="projects-img">
+							<img src="<?php echo get_template_directory_uri() . '/images/service1.jpg'; ?>" />	
+						</div>
+						<div class="projects-cont">
+							<h5>Tree condition reports</h5>
+					<p>Maecenas et est at justo condimentum elementum. Maecenas velit nisi, tempus ac metus at, vehicula imperdiet augue. Morbi posuere lectus nec ex ornare bibendum.</p>
+					<div class="kt-lst">
+							<ul>
+								<li>Praesent a libero in nulla convallis</li>
+								<li>Tincidunt a id lorem mauris lobortis lobortis</li>
+								<li>Morbi sem augue, condimentum quis </li>
+							</ul>						</div>
+							<div class="abtn-btn">
+								<a href="#">Learn More</a>
+							</div>
+						</div>
+					</div>
+										<div class="projects-col">
+						<div class="projects-img">
+							<img src="<?php echo get_template_directory_uri() . '/images/service2.jpg'; ?>" />	
+						</div>
+						<div class="projects-cont">
+							<h5>Surveys for development</h5>
+					<p>Maecenas et est at justo condimentum elementum. Maecenas velit nisi, tempus ac metus at, vehicula imperdiet augue. Morbi posuere lectus nec ex ornare bibendum.</p>
+					<div class="kt-lst">
+							<ul>
+								<li>Praesent a libero in nulla convallis</li>
+								<li>Tincidunt a id lorem mauris lobortis lobortis</li>
+								<li>Morbi sem augue, condimentum quis </li>
+							</ul>						</div>
+							<div class="abtn-btn">
+								<a href="#">Learn More </a>
+							</div>
+						</div>
+					</div>
+										<div class="projects-col">
+						<div class="projects-img">
+							<img src="<?php echo get_template_directory_uri() . '/images/service3.jpg'; ?>" />
+						</div>
+						<div class="projects-cont">
+							<h5>Woodland anagement</h5>
+					<p>Maecenas et est at justo condimentum elementum. Maecenas velit nisi, tempus ac metus at, vehicula imperdiet augue. Morbi posuere lectus nec ex ornare bibendum.</p>
+					<div class="kt-lst">
+							<ul>
+								<li>Praesent a libero in nulla convallis</li>
+								<li>Tincidunt a id lorem mauris lobortis lobortis</li>
+								<li>Morbi sem augue, condimentum quis </li>
+							</ul>						</div>
+							<div class="abtn-btn">
+								<a href="#">Learn More</a>
+							</div>
+						</div>
+					</div>
+					
+		</div>
+	</div>
+</div>
+</div>
+
+
+
+
+
+
+
+<div class="container">
+					<div class="home-bring">
+
+<div class="bring-img">
+				<img src="<?php echo get_template_directory_uri() . '/images/need-ser.png'; ?>" />
+			</div>
+
+			<div class="bring-contant">
+				
+				<div class="all-heading">
+					<h2>Who Needs our Service</h2>
+				</div>
+
+				
+			<p>Donec augue ex, vestibulum a ex eget, aliquam porttitor nisi. Nunc tempus cursus ligula, non blandit nulla aliquam ac. Nullam mollis gravida mi eget placerat. Duis iaculis porttitor nunc, ac dignissim felis iaculis eu. </p>	
+			<p>Nullam ultrices imperdiet sagittis. Duis sed turpis et orci fermentum vestibulum. In porta felis vel ante mattis congue. Phasellus at felis ac dui pellentesque mattis consectetur vel ipsum. Curabitur ac semper nunc. Donec elementum condimentum neque, quis tempus mauris. Fusce suscipit metus a nisl porttitor, nec commodo dui porta.</p>	
+			<div class="all-btn">
+					<a href="#">Contact Us</a>
+				</div>		
+			</div>
+			
+
+			
+		</div>
+	
+
+</div>
+
+<div class="client-sey"><
+		<div class="all-heading">
+					<h2>What our client say</h2>
+				</div>
+
+	
+		</div>
+
+<div class="form-section">
+  <div class="container">
+        <div class="static-form">
+            <h4>Request a consultation</h4>
+           <?php echo do_shortcode('[contact-form-7 id="0d69efd" title="Contact-form 1"]'); ?>
+
+       </div>
+   </div>
+</div>
+
+	<?php
+	get_footer();
