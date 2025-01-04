52f5126c21c74401e49a8773d9a7f6fd115cebaf
Mitanshi Gaur <118531410+mitanshigaur09@users.noreply.github.com>
2025-01-04 20:24:45 +0530

    Add files via upload

---
+++ IRT/home.php
@@ -0,0 +1,219 @@
+<?php 
+
+/*Template Name: Home */
+
+get_header();
+
+?>
+
+
+<div class="hero-section">
+	<div id="hero-slider" class="owl-carousel owl-loaded owl-drag">
+		<div class="home_bnr">
+			<img src="<?php echo get_template_directory_uri() . '/images/irt_bnr.jpg'; ?>" />
+			<div class="container">
+				<div class="home_content">
+					
+					<div class="bnr_heading">
+						<h1>IRT Consult </h1>
+						<h2>Environmental & Energy Ltd</h2>
+					</div>
+					<p>Alienum phaedrum torquatos nec eu vis detraxit</p>
+                          <div class="bnr_list">
+					          <ul>
+						         <li>Lorem Ipsum is simply dummy</li>
+						          <li>Contrary to popular belief</li>
+						          <li>The standard chunk of Lorem</li>
+					           </ul>
+					       </div>
+					       
+					<div class="bnr_button">
+					<a href="">Request a consultation<i class="fas fa-arrow-right"></i></a>
+					</div>
+					 
+				</div>
+			</div>
+		</div>
+	<div class="home_bnr">
+			<img src="<?php echo get_template_directory_uri() . '/images/irt_bnr.jpg'; ?>" />
+			<div class="container">
+				<div class="home_content">
+					
+					<div class="bnr_heading">
+						<h1>IRT Consult </h1>
+						<h2>Environmental & Energy Ltd</h2>
+					</div>
+					<p>Alienum phaedrum torquatos nec eu vis detraxit</p>
+                          <div class="bnr_list">
+					          <ul>
+						         <li>Lorem Ipsum is simply dummy</li>
+						          <li>Contrary to popular belief</li>
+						          <li>The standard chunk of Lorem</li>
+					           </ul>
+					       </div>
+					       
+					<div class="bnr_button">
+					<a href="">Request a consultation<i class="fas fa-arrow-right"></i></a>
+					</div>
+					 
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+
+
+<div class="container">
+
+	<!-- <div class="strip-section"> -->
+		<div id="icon-script" class="owl-carousel owl-loaded owl-drag">
+		<div class="strip-block">
+			<img src="<?php echo get_template_directory_uri() . '/images/badge1.png'; ?>" />
+			<p>Lorem ipsum dolor</p>
+		</div>
+		<div class="strip-block">
+			<img src="<?php echo get_template_directory_uri() . '/images/badge2.png'; ?>" />
+			<p>Lorem ipsum dolor</p>
+		</div>
+		<div class="strip-block">
+			<img src="<?php echo get_template_directory_uri() . '/images/badge1.png'; ?>" />
+			<p>Lorem ipsum dolor</p>
+		</div>
+	</div>
+<!-- </div> -->
+</div>
+
+
+
+<div class="container">
+	<div class="projects-section">
+		<div class="all-heading"><h3>Key Services</h3></div>
+		<div class="projects-row">
+			<div class="projects-col">
+				<div class="projects-img">
+					<img src="<?php echo get_template_directory_uri() . '/images/ppt1.jpg'; ?>" />
+				</div>
+				<div class="projects-cont">
+					<h5>Pallet Racking Inspections</h5>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took.</p>
+<div class="kt-lst">
+								<ul>
+								<li>Lorem Ipsum is simply</li>
+								<li>Lorem Ipsum is simply</li>
+								<li>Lorem Ipsum is simply</li>
+							</ul>							</div>
+					<div class="abtn-btn">
+						<a href="#">Read More<i class="fas fa-arrow-right"></i></a>
+					</div>
+				</div>
+			</div>
+			<div class="projects-col">
+				<div class="projects-img">
+					<img src="<?php echo get_template_directory_uri() . '/images/ppt2.jpg'; ?>" />
+				</div>
+				<div class="projects-cont">
+					<h5>Gas Leak Detection</h5>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took.</p>
+<div class="kt-lst">
+								<ul>
+								<li>Lorem Ipsum is simply</li>
+								<li>Lorem Ipsum is simply</li>
+								<li>Lorem Ipsum is simply</li>
+							</ul>							</div>
+					<div class="abtn-btn">
+						<a href="#">Read More<i class="fas fa-arrow-right"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<div class="container">
+	<div class="big_triangle">
+	<div class="triangle">
+		<div class="first_triangle">
+			<img src="<?php echo get_template_directory_uri() . '/images/tri.png'; ?>" />
+			<div class="triangle_content">
+				<div class="triangle_heading">
+					<h4>Training in <br>
+Pallet Racking Inspections </h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br> Donec rhoncus porta tortor vel vulputate.</p>
+				</div>
+				<div class="trained">
+						<a href="#">Training<i class="fas fa-arrow-right"></i></a>
+					</div>
+			</div>
+		</div>
+		<div class="second_triangle">
+			<img src="<?php echo get_template_directory_uri() . '/images/true.jpg'; ?>" />
+		</div>
+	</div>
+</div>
+</div>
+
+
+
+
+
+
+
+
+<div class="container">
+	<div class="complete-row complete-bun">
+		<div class="complete-content">
+			<div class="all-heading">
+				<h5>About Our Comapny</h5>
+				<h3>Welcome to IRT Consult<br> Environmental & Energy Ltd</h3>
+			</div>
+			<p>IRT Consult Environmental & Energy Ltd are proud to announce that we are now providing VOC (Volatile Organic Compound) Leak detection using Infrared Technology.
+            Our service uses state of the art imaging equipment to visually show the location of fugitive emissions or leaks.</p>
+			<div class="list-section">
+				<ul>
+					<li>Praesent a libero in nulla convallis</li>
+					<li>Tincidunt a id lorem mauris lobortis lobortis</li>
+					<li>Morbi sem augue, condimentum quis</li>
+				</ul>				
+			</div>
+			<div class="tust">
+						<div class="trusted-img">
+							<img src="<?php echo get_template_directory_uri() . '/images/batch1.jpg'; ?>" />
+						</div>
+						<div class="trusted-img">
+							<img src="<?php echo get_template_directory_uri() . '/images/batch2.jpg'; ?>" />
+						</div>
+						<div class="trusted-img">
+							<img src="<?php echo get_template_directory_uri() . '/images/batch3.jpg'; ?>" />
+						</div>
+					</div>
+				<div class="abt-button">
+					<a data-toggle="modal" data-target="#exampleModal" href="#">Read more<i class="fas fa-arrow-right"></i></a>
+				</div>
+			</div>
+			<div class="complete-images bun">
+				<img src="<?php echo get_template_directory_uri() . '/images/blackboy.jpg'; ?>" />
+			</div>
+		</div>
+	</div>
+
+
+
+<div class="container">
+	<div class="static-form">
+		<div class="all-heading">
+		<h3>Request a Consultation</h3>
+	</div>
+		<?php echo do_shortcode('[contact-form-7 id="0ccf129" title="Contact-form 1"]'); ?>
+
+	</div>
+</div>
+
+<?php 
+
+get_footer()
+
+?>
